d6381f0a5279e969c274ca0b2c12b7504923cc13
diegoefp <diegoefp_90@hotmail.com>
2024-10-31 22:26:35 -0300

    models 31-10

--- poesias/admin.py
+++ poesias/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 # Register your models here.
+from .models import Category, Author, Book
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date',)
+    list_filter = ('author', 'categories', 'published_date',)
+    search_fields = ('title', 'author__name', 'categories__name',)
+    readonly_fields = ('cover_preview',)
+
+    def cover_preview(self, obj):
+        return mark_safe(f'<img src="{obj.cover.url}" width="100" />')
+    cover_preview.short_description = "Cover Preview"
+
+
+# admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Author)
+# admin.site.register(Book)
--- poesias/models.py
+++ poesias/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+    cover = models.ImageField(upload_to='book_covers/')
+
+    def __str__(self):
+        return self.title
+
+
